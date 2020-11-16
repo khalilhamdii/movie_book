299550d05b9bb27cfb3be7c189397853aae24304
khalilhamdii <khaalil.hamdi@gmail.com>
2020-11-16 16:44:28 +0100

    Add styling to navbar and footer using mdb packages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
   def nav_buttons
     if logged_in?
-      content_tag :div do
-        content_tag(:button, current_user.name, onclick: "location.href = '../users';", class: 'btn btn-success mr-2') +
-          content_tag(:button, 'Logout', onclick: "location.href = '../logout';", class: 'btn btn-success')
+      content_tag :div, class: 'dropdown-menu dropdown-menu-right dropdown-info' do
+        content_tag(:a, current_user.name, href: users_path, class: 'dropdown-item') +
+          content_tag(:a, 'Logout', href: logout_path, class: 'dropdown-item')
       end
     else
-      content_tag :div do
-        content_tag(:button, 'Sign Up', onclick: "location.href = '../signup';", class: 'btn btn-success mr-2') +
-          content_tag(:button, 'Login', onclick: "location.href = '../login';", class: 'btn btn-success')
+      content_tag :div, class: 'dropdown-menu dropdown-menu-right dropdown-info' do
+        content_tag(:a, 'Signup', href: signup_path, class: 'dropdown-item') +
+          content_tag(:a, 'Login', href: login_path, class: 'dropdown-item')
       end
     end
   end
