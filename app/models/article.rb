f35a6c6547a0e3f34c7e4cbfaf199355cfc796ae
khalilhamdii <khaalil.hamdi@gmail.com>
2020-11-17 14:13:23 +0100

    Add categories input to article form

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,4 +6,16 @@
     has_many :votes, dependent: :destroy
     has_many :art_cats
     has_many :categories, through: :art_cats
+
+    def category_list
+      self.categories.collect do |category|
+        category.name
+      end.join(", ")
+    end
+
+    def category_list=(categories_string)
+      category_names = categories_string.split(",").collect{|s| s.strip.downcase}.uniq
+      new_or_found_categories = category_names.collect { |name| Category.find_or_create_by(name: name) }
+      self.categories = new_or_found_categories
+    end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
   has_many :art_cats
   has_many :articles, through: :art_cats
+
+  def to_s
+    name
+  end
 end
