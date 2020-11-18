6c4da48c023e5e458d984d1e8d603b8a412aed27
khalilhamdii <khaalil.hamdi@gmail.com>
2020-11-18 19:21:25 +0100

    Add styling to notice + visual adjustments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
     @categories = Category.all
     @articles = Article.limit(2)
     @item = Article.last
+    @time = Time.new
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
+
+  def notice_bar
+    if notice
+      content_tag(:div, notice, class: 'amber darken-4 text-white text-center rounded-pill font-weight-bold p-2 m-2')
+    end
+  end
+
   def nav_buttons
 
     if logged_in?
@@ -6,7 +13,7 @@ module ApplicationHelper
         image_tag(current_user.avatar, alt: "avatar image", class: "rounded-circle z-depth-0 avatar-img")
       end
       content_tag(:div,avatar_link,class: 'nav-item') + 
-        content_tag(:div, (link_to "#{current_user.name}", users_path, class: " brown-text font-weight-bold ml-2 mr-4"), class: 'nav-item') +
+        content_tag(:div, (link_to "#{current_user.name.upcase}", users_path, class: " brown-text font-weight-bold ml-2 mr-4"), class: 'nav-item') +
           content_tag(:div,(link_to "LOGOUT", logout_path, class: " brown-text font-weight-bold mr-5"), class: 'nav-item')
     else
         content_tag(:div, (link_to "LOGIN", login_path, class: " brown-text font-weight-bold mx-4"), class: 'nav-item') +
