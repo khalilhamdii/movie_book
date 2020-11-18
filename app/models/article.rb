d4e86bbb3c84e1f5fd963cf5891db1146bf1f7ca
khalilhamdii <khaalil.hamdi@gmail.com>
2020-11-18 17:38:17 +0100

    Add validation methods for uploading images

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,6 +7,7 @@
     has_many :art_cats, dependent: :destroy
     has_many :categories, through: :art_cats, dependent: :destroy
     has_one_attached :image, dependent: :destroy
+    validate :image_format
 
     def category_list
       self.categories.collect do |category|
@@ -19,4 +20,30 @@
       new_or_found_categories = category_names.collect { |name| Category.find_or_create_by(name: name) }
       self.categories = new_or_found_categories
     end
+
+    
+  
+def resize_image
+    resized_image = MiniMagick::Image.read(image.download)
+    resized_image = resized_image.resize "400x400"
+    v_filename = image.filename
+    v_content_type = image.content_type
+    image.purge
+    image.attach(io: File.open(resized_image.path), filename:  v_filename, content_type: v_content_type)
+end
+private
+def image_format
+ return unless image.attached?
+ if image.blob.content_type.start_with? 'image/'
+   if image.blob.byte_size > 10.megabytes
+     errors.add(:image, 'size needs to be less than 10MB')
+     image.purge
+   else
+     resize_image
+   end
+  else
+    image.purge
+    errors.add(:image, 'needs to be an image')
+  end
+end
 end
